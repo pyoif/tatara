4cb3af3ea664b8cb5c083de672544d9ef4b26dd0
Hiyurigi <kafetheine@gmail.com>
2026-07-09 08:37:11 +0700

    test: add JUnit 5 test infrastructure

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,13 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 gradlePlugin {
---
+++ src/test/kotlin/com/pyoif/tatara/SmokeTest.kt
@@ -0,0 +1,11 @@
+package com.pyoif.tatara
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class SmokeTest {
+    @Test
+    fun `test framework works`() {
+        assertTrue(true)
+    }
+}
